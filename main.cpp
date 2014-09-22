2a2373afa4505d859c4d967cfe87e2556aacfd2a
Nathan Hernandez <email@nathanph.com>
2014-09-22 11:58:33 -0400

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Nate
+ *
+ * Created on September 22, 2014, 11:24 AM
+ */
+
+#include <cstdlib>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    wiringPiSetup();
+    return 0;
+}
+
